0c4d8c472de893d9eecefb90d80152bafadc07ad
Bernhard Reiter <bernhard@intevation.de>
2016-11-16 15:48:36 +0100

    Adds missing subcomponents
    Tickets, Contacts, Stats.

---
+++ src/components/dash/Contacts.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="content">
+    <div class="row center-block">
+      <h2>Empty</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+// import $ from 'jquery'
+
+module.exports = {
+  name: 'Contacts',
+  mounted: function () {
+  }
+}
+</script>
+
+<style>
+</style>
