a1fd79dd802545ef9b4d465ae1be46906c5ced67
Bernhard Reiter <bernhard@intevation.de>
2016-12-07 17:08:49 +0100

    Bumping version number

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,7 +11,7 @@
     data: function () {
       return {
         section: 'Head',
-        version: '0.1.0',
+        version: '0.2.0dev',
         callingAPI: false,
         serverURI: 'http://localhost:8080',
         caller: this.$http
