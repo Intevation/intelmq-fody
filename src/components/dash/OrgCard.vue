438ec57fd9b2e834b7ff97b6ab335a3661643421
Bernhard Reiter <bernhard@intevation.de>
2017-02-14 11:49:21 +0100

    Contacts: cleanup OrgCard.
     * Removes superfluous template value, which is not used and was left
       over when turned into a single file component.

--- src/components/dash/OrgCard.vue
+++ src/components/dash/OrgCard.vue
@@ -37,37 +37,6 @@
 module.exports = {
   name: 'org-card',
   props: ['status', 'org'],
-  template: ` <div class="panel panel-primary">
-                <div class="panel-heading">
-                  <h3 class="panel-title">
-                    <i class="fa fa-address-book-o rme"></i>
-                    {{ org.name }}
-                    <span class="badge primary">{{ status }}</span>
-                  </h3>
-                </div>
-                <div class="panel-body">
-                  <ul class="list-group">
-                    <li v-for="contact of org.contacts" class="list-group-item">
-                      <i class="fa fa-envelope-o rme"></i>
-                      {{ contact.email }}
-                      <em v-if="contact.comment !== ''">
-                        ({{ contact.comment }})
-                      </em>
-                    </li>
-                  </ul>
-                  <ul class="list-group">
-                    <li v-for="asn of org.asns" class="list-group-item">
-                      <i class="fa fa-hdd-o rme"></i>
-                      ASN{{ asn.number }}
-                    </li>
-                  </ul>
-                  <div class="well">
-                    <div v-for="(value, key) in otherAttributes">
-                      <strong>{{ key }}</strong>: {{ value }}
-                    </div>
-                  </div>
-                </div>
-              </div>`,
   data: function () {
     return {
       // for knownOrgKeys, the display is handled explicitely
