f88f22037da4ac7d1ef49ea545358513baf90f1f
Bernhard Reiter <bernhard@intevation.de>
2017-04-06 16:37:59 +0200

    Version: bumbed to 0.4.1-dev

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,7 +11,7 @@
     data: function () {
       return {
         section: 'Head',
-        version: '0.4.0',
+        version: '0.4.1-dev',
         callingAPI: false,
         serverURI: 'http://localhost:8080',
         caller: this.$http
