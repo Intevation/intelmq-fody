8dd53b1573b52f2fe54c408cad4b2fa2907e08c8
Bernhard Reiter <bernhard@intevation.de>
2018-02-09 11:14:48 +0100

    Improves <input-unsigned-int> component.
     * Adds necessary checks so that unwanted input leads to behaviour that
       users will accept. Therefore we keep the last valid value and jump
       back to it under some circumstances.

--- src/components/dash/InputUnsignedInt.vue
+++ src/components/dash/InputUnsignedInt.vue
@@ -1,9 +1,16 @@
 <template>
-  <input type="text" v-bind:value="value"
+  <input ref="input" type="text" v-bind:value="value"
     v-on:input="updateValue($event.target.value)"></input>
   </span>
 </template>
 <script>
+// Documentation:
+// This component allows entering
+// unsigned integers [0, 1, 2, .., Number.MAX_VALUE] or ''
+// via a text input field; not offering increment and decrement functionality.
+// It will go back to the last valid input, if a different char is entered
+// and this may cause the cursor to jump to the end of the value, because
+// the cursor position is not retained.
 module.exports = {
   name: 'input-unsigned-int',
   props: {
@@ -11,12 +18,28 @@ module.exports = {
   },
   data: function () {
     return {
+      previousValue: (/^[0-9]+$/.test(this.value)
+                      ? Number.parseInt(this.value, 10) : '')
     }
   },
   methods: {
     updateValue: function (value) {
-      // TODO add checks that we only return an unsigned integer.
-      this.$emit('input', Number(value))
+      var sanitizedValue = Number.parseInt(value, 10)
+
+      if (value === '') {
+        sanitizedValue = ''
+      } else if (!/^[0-9]+$/.test(value)) {
+        sanitizedValue = this.previousValue
+      }
+
+      this.previousValue = sanitizedValue
+
+      // see vue guide section: # Form Input Components using Custom Events
+      if (sanitizedValue !== Number(value)) {
+        this.$refs.input.value = sanitizedValue
+      }
+
+      this.$emit('input', sanitizedValue)
     }
   }
 }
