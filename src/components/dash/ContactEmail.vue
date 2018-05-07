49c4f0c9dbc1875e465f825890cf3b36d7cd75e8
Bernhard Herzog <bernhard.herzog@intevation.de>
2018-05-07 17:32:26 +0200

    Contact: Add component for showing a contact's email address
    This component is basically a copy of the email part of OrgCard.vue.

---
+++ src/components/dash/ContactEmail.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <div class="col-sm-1 col-xs-1"
+       ><i class="fa fa-envelope-o"></i></div>
+  <div class="col-sm-11 col-xs-11">
+    {{ firstname }} {{ lastname }} &lt;{{ email }}&gt;
+    <small>
+      <toggle-button v-model="enabled" :width=36 :height=14
+                     :labels="{checked: 'on', unchecked: 'off'}"
+                     :color="{checked: '#d4d4d4', unchecked: '#d73925'}"/>
+    </small>
+    <em v-if="comment !== ''">({{ comment }})</em>
+  </div>
+</div>
+</template>
+
+<script>
+module.exports = {
+  name: 'contact-email',
+  props: {
+    'firstname': String,
+    'lastname': String,
+    'email': String,
+    'comment': String
+  },
+  data: function () {
+    return {
+      enabled: true
+    }
+  }
+}
+</script>
+
+<style>
+i.rme {
+  margin-right: 0.25em
+}
+i.lme {
+  margin-left: 0.35em
+}
+</style>
