7e5a4c4a57296d9d43c11f1db5e3758fd6e76318
Jenny Krambach <jenny.krambach@intevation.de>
2021-11-15 01:46:22 +0100

    remove spinner
      eye candy only, pulling unnecessary dependencies

--- build/build.js
+++ build/build.js
@@ -5,7 +5,6 @@ env.NODE_ENV = 'production'
 
 var path = require('path')
 var config = require('../config')
-var ora = require('ora')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
 
@@ -15,16 +14,12 @@ console.log(
   '  Opening index.html over file:// won\'t work.\n'
 )
 
-var spinner = ora('building for production...')
-spinner.start()
-
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
 
 webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
   if (err) throw err
   process.stdout.write(stats.toString({
     colors: true,
